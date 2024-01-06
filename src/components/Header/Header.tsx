76bc73d222c3b9fb00d25427049066373fdcbf23
BrunoTrindade40 <brunotrindade_40@hotmail.com>
2024-01-06 13:02:22 -0300

    Parte 1 do Desafio

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,27 @@
-import './Header.css'
+//import './Header.css'
+
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Flex,
+  Heading,
+  Spacer
+} from '@chakra-ui/react';
 
 export const Header  = () => {
   return(
-    <div className='header'>
-      Dio Bank
-    </div>
+    <>
+      <Flex alignItems='center' gap={2} bgColor='white' minWidth='100%' padding='0 25px'>
+        <Box p={2}>
+          <Heading>DIO Bank</Heading>
+        </Box>
+        <Spacer />
+        <ButtonGroup gap='2'>
+          <Button colorScheme='teal'>Sign Up</Button>
+          <Button colorScheme='teal'>Log in</Button>
+        </ButtonGroup>
+      </Flex>
+    </>
   )
 }
